eb812f216cb5d60bd9d19794fe839825200a2c2a
Sahana <sahanaselvasekar11@gmail.com>
2026-07-17 21:00:57 +0530

    Fix:route

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 //get the input from user
-app.post('api/Depreciation_Calculator ', (req, res) => {
+app.post('/api/calculate_asset_depreciation ', (req, res) => {
   const { cost, salvageValue, year } = req.body;
   // write validations
 
   if (savageValue >= cost) return res.status(400).json({ error: "Salvage value must be less than asset cost" });
-  if (year > 0) return res.status(400).json({ error: "Duration must be greater than 0" });
+  if (year <= 0) return res.status(400).json({ error: "Duration must be greater than 0" });
 
   // use the formula to calculate depreciation - stright line method
   //- yearlyDepreciation = (costOfAsset - salvageValue) / duration
@@ -16,5 +17,5 @@ app.post('api/Depreciation_Calculator ', (req, res) => {
   //return the depreciation value in clean json structure
 
   res.send(depreciated_value)
-  app.listen(3000);
   const port = process.env.port || 3000;
+  app.listen(port, () => console.log(`Server running on port ${port}`));
