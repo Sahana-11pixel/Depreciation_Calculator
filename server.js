9424a63db7d65271578bf709268645541794b195
Sahana <sahanaselvasekar11@gmail.com>
2026-07-17 20:38:07 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+let cost,savageValue,year;
+//get the input from user
+app.post('api/Depreciation_Calculator ',(req,res) => {
+      req.body(cost,savageValue,year);
+      // write validations
+
+      if (savageValue < cost ) return res.send(req.body(cost,savageValue,year))
+        else 
+    return error("Savage value should be less than asset cost");
+if (year<0) return  res.send(req.body(cost,savageValue,year))
+});
+
+// use the formula to calculate depreciation - stright line method
+//- yearlyDepreciation = (costOfAsset - salvageValue) / duration
+const depreciated_value = (cost - savageValue) / year;
+
+//return the depreciation value in clean json structure
+
+res.send (depreciated_value)
+app.listen(3000);
+const port = process.env.port || 3000;
