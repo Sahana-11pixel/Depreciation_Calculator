22633398c66e54ca063f6652ce91f15e51569a16
Sahana <sahanaselvasekar11@gmail.com>
2026-07-17 20:50:41 +0530

    Fix:Destructuring

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
 
-let cost,savageValue,year;
 //get the input from user
-app.post('api/Depreciation_Calculator ',(req,res) => {
-      req.body(cost,savageValue,year);
-      // write validations
+app.post('api/Depreciation_Calculator ', (req, res) => {
+  const { cost, salvageValue, year } = req.body;
+  // write validations
 
-      if (savageValue < cost ) return res.send(req.body(cost,savageValue,year))
-        else 
+  if (savageValue < cost) return res.send(req.body(cost, savageValue, year))
+  else
     return error("Savage value should be less than asset cost");
-if (year<0) return  res.send(req.body(cost,savageValue,year))
+  if (year < 0) return res.send(req.body(cost, savageValue, year))
 });
 
 // use the formula to calculate depreciation - stright line method
@@ -19,6 +18,6 @@ const depreciated_value = (cost - savageValue) / year;
 
 //return the depreciation value in clean json structure
 
-res.send (depreciated_value)
+res.send(depreciated_value)
 app.listen(3000);
 const port = process.env.port || 3000;
