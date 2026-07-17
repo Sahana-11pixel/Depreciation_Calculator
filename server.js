10702dbaeaeaab89374aca2f077b125fc52e20c1
Sahana <sahanaselvasekar11@gmail.com>
2026-07-17 20:56:27 +0530

    Fix:Condition

--- server.js
+++ server.js
@@ -6,18 +6,15 @@ app.post('api/Depreciation_Calculator ', (req, res) => {
   const { cost, salvageValue, year } = req.body;
   // write validations
 
-  if (savageValue < cost) return res.send(req.body(cost, savageValue, year))
-  else
-    return error("Savage value should be less than asset cost");
-  if (year < 0) return res.send(req.body(cost, savageValue, year))
-});
+  if (savageValue >= cost) return res.status(400).json({ error: "Salvage value must be less than asset cost" });
+  if (year > 0) return res.status(400).json({ error: "Duration must be greater than 0" });
 
-// use the formula to calculate depreciation - stright line method
-//- yearlyDepreciation = (costOfAsset - salvageValue) / duration
-const depreciated_value = (cost - savageValue) / year;
+  // use the formula to calculate depreciation - stright line method
+  //- yearlyDepreciation = (costOfAsset - salvageValue) / duration
+  const depreciated_value = (cost - savageValue) / year;
 
-//return the depreciation value in clean json structure
+  //return the depreciation value in clean json structure
 
-res.send(depreciated_value)
-app.listen(3000);
-const port = process.env.port || 3000;
+  res.send(depreciated_value)
+  app.listen(3000);
+  const port = process.env.port || 3000;
